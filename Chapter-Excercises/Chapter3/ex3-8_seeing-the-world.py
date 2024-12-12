e1e864935cc4a2a999ce93009a17f00a0a55d8d2
Simeon Cargile <cargileaws@gmail.com>
2024-12-12 15:51:52 -0500

    add excercise 3-8

---
+++ Chapter-Excercises/Chapter3/ex3-8_seeing-the-world.py
@@ -0,0 +1,34 @@
+# Create list of places to visit
+places_to_visit = ['Tokyo', 'New York', 'California', 'Texas', 'Chicago']
+
+
+# Print list in original order
+print(places_to_visit)
+print()
+# Print list in alphabetical order
+print(sorted(places_to_visit))
+print()
+# Print list to confirm that it's still in the original order
+print(places_to_visit)
+print()
+# Print list in reverse-alphabetical order
+print(sorted(places_to_visit, reverse=True))
+print()
+# Print list to confirm that it's still in the original order
+print(places_to_visit)
+print()
+# Reverse the order of the list
+places_to_visit.reverse()
+print(places_to_visit)
+print()
+# Reverse the order of the list again
+places_to_visit.reverse()
+print(places_to_visit)
+print()
+# Print list so it's stored in alphabetical order
+places_to_visit.sort()
+print(places_to_visit)
+print()
+# Print list so it's stored in reverse-alphabetical order
+places_to_visit.sort(reverse=True)
+print(places_to_visit)
