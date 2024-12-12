086ca58b8543e07693235d06d497443aad9dcd4b
Simeon Cargile <cargileaws@gmail.com>
2024-12-12 16:32:54 -0500

    add excercise 4-2

---
+++ Chapter-Excercises/Chapter4/ex4-2_animals.py
@@ -0,0 +1,6 @@
+animals = ['lion', 'cheetah', 'panther']
+
+for animal in animals:
+    print(f"A {animal} would not make a good pet.")
+print()
+print("Any of these animals would be dangerous to have as a pet")
