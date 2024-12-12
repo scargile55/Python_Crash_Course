32c03ae4f41802541385c88dc0a902784b4136b3
Simeon Cargile <cargileaws@gmail.com>
2024-12-12 17:38:29 -0500

    add excercise excercise4-10

---
+++ Chapter-Excercises/Chapter4/ex4-10_slices.py
@@ -0,0 +1,10 @@
+football_teams = ['Falcons', 'Ravens', 'Packers', 'Colts', 'Panthers', 'Eagles', 'Saints']
+
+# Prints the first three teams using 'slice'
+print(f"The first three items are: {football_teams[:3]}")
+print()
+# Prints the three teams in the middle
+print(f"The three teams in the middle of the list are: {football_teams[2:5]}")
+print()
+# Print the last three items
+print(f"The last three items in the list are: {football_teams[4:]}")
