3df2469506406ac3255c853828953a77415658d1
Simeon Cargile <cargileaws@gmail.com>
2024-12-12 14:50:04 -0500

    add excercise 3-4

---
+++ Chapter-Excercises/Chapter3/ex3-4_guest-list.py
@@ -0,0 +1,6 @@
+# Create a list of people and print a message to invite them to dinner.
+guest_list = ['playboi carti', 'kanye', 'kendrick lamar']
+
+print(f"{guest_list[0].title()} is invited to my dinner.")
+print(f"{guest_list[1].title()} is invited to my dinner.")
+print(f"{guest_list[2].title()} is invited to my dinner.")
