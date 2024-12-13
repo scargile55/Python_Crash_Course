93f051968d15fd0163c6c23908519c05028a5717
Simeon Cargile <cargileaws@gmail.com>
2024-12-12 23:58:11 -0500

    add excercise5-6

---
+++ Chapter-Excercises/Chapter5/ex5-6_favorite-fruit.py
@@ -0,0 +1,13 @@
+favorite_fruit = ['grapes', 'kiwis', 'watermelon']
+
+for fruit in favorite_fruit:
+    if 'grapes' in favorite_fruit:
+        print(f"You really like {fruit}")
+    elif 'kiwis' in favorite_fruit:
+        print(f"You really like {fruit}")
+    elif 'pineapples' in favorite_fruit:
+        print(f"You really like {fruit}")
+    elif 'peaches' in favorite_fruit:
+        print(f"You really like {fruit}")    
+    elif 'watermelon' in favorite_fruit:
+        print(f"You really like {fruit}")
