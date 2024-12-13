4657a4a0fde1e7a236b732789ec32b786f9e4a58
Simeon Cargile <cargileaws@gmail.com>
2024-12-13 17:18:32 -0500

    add excercise6-3

---
+++ Chapter-Excercises/Chapter6-Dictionaries/ex6-3_rivers.py
@@ -0,0 +1,8 @@
+rivers = {
+    'nile': 'egypt',
+    'mississippi': 'usa',
+    'amazon': 'south america'
+}
+
+for k,v in rivers.items():
+    print(f"The {k.title()} river runs through {v.title()}")
