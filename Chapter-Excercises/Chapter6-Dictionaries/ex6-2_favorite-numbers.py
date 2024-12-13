a343b88df9d132ccef5612e248969058f4a62915
Simeon Cargile <cargileaws@gmail.com>
2024-12-13 17:18:12 -0500

    add excercise6-2

---
+++ Chapter-Excercises/Chapter6-Dictionaries/ex6-2_favorite-numbers.py
@@ -0,0 +1,10 @@
+favorite_numbers = {
+    'Bea': 7,
+    'Nick': 10,
+    'Micah': 2,
+    'Lola': 8,
+    'Peta': 9
+}
+
+for k,v in favorite_numbers.items():
+    print(f"{k}'s favorite number is {v}.")
